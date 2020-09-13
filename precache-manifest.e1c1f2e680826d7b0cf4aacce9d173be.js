3d5d36ed5a864663b5f83c23d83c1032d9743d60
JAGANSRGDM <jagansrgdm@gmail.com>
2020-09-13 11:18:28 +0530

    Updates

---
+++ precache-manifest.e1c1f2e680826d7b0cf4aacce9d173be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd32e35de8fe3711487a",
+    "url": "/Myapp/static/css/main.6a04f8cf.chunk.css"
+  },
+  {
+    "revision": "fd32e35de8fe3711487a",
+    "url": "/Myapp/static/js/main.8cdd701a.chunk.js"
+  },
+  {
+    "revision": "cc36a756c4f4e5020823",
+    "url": "/Myapp/static/js/runtime~main.36717d5b.js"
+  },
+  {
+    "revision": "492f1ec7bc4927976c82",
+    "url": "/Myapp/static/js/2.ace4a8e2.chunk.js"
+  },
+  {
+    "revision": "8d2242bb04e5246c34ba1045a0aa22a4",
+    "url": "/Myapp/index.html"
+  }
+];
